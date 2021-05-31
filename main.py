c3bc5ddd258a14a1198e790bf1060708b5fb0892
ibnshabir <ibnshabir.ali@gmail.com>
2021-05-31 08:10:32 -0700

    uncommented main's code

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-#
-# from PyQt5.QtWidgets import QApplication
-# import sys
-#
-# from display_window import DisplayWindow
-# from controller import Controller
-# from model import Model
-# from context import Context
-#
-#
-# app = QApplication(sys.argv)
-# context = Context()
-# model = Model(context)
-# controller = Controller(model)
-# display_window = DisplayWindow(controller, model)
-# sys.exit(app.exec())
 
-while(True):
-    print('hi')
+from PyQt5.QtWidgets import QApplication
+import sys
+
+from display_window import DisplayWindow
+from controller import Controller
+from model import Model
+from context import Context
+
+
+app = QApplication(sys.argv)
+context = Context()
+model = Model(context)
+controller = Controller(model)
+display_window = DisplayWindow(controller, model)
+sys.exit(app.exec())
